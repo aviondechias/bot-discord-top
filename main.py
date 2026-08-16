758df5d9deb4a5fb56e2ef824d68ace2f19bcab9
aviondechias <mathiskenney1@gmail.com>
2026-08-16 12:30:41 -0400

    Change Discord bot token
    Updated bot token for Discord bot connection.

--- main.py
+++ main.py
@@ -110,4 +110,4 @@ async def on_ready():
 
 # Lancement du mini-site web puis du bot Discord
 keep_alive()
-bot.run("MTUzODU1MTI5Nzk4NzMyMTk1OQ.GyDP9S.6QxjeNCcCguCDkuXktoeUzL4OywMwrf9uuoHGk")
+bot.run("MTUzODU1MTI5Nzk4NzMyMTk1OQ.GvIFmX.ShUCCHeKUGjUq_ucSndbDFnu_pajLY7r6FQj-E")
